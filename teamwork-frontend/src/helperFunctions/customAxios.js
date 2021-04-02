ed4ad90c889d6193646c97279eedef6f65a37bd6
MOJEED KUSIMO <mkusimo90@gmail.com>
2021-04-02 08:22:17 +0100

    Changed api url

--- teamwork-frontend/src/helperFunctions/customAxios.js
+++ teamwork-frontend/src/helperFunctions/customAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://andela-teamwork.herokuapp.com/api'
 });
 // https://andela-teamwork.herokuapp.com/api
 // http://localhost:5000/api
